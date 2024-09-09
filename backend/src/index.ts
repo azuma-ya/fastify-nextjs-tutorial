ecc013183c60ce07ad2d1e7db91e920ea1e37dc6
azuma-ya <kazu.bluemountain@gmail.com>
2024-09-10 08:23:57 +0900

    init

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,6 @@
-import { PrismaClient } from "@prisma/client";
 import Fastify, { FastifyRequest, FastifyReply } from "fastify";
 
 const fastify = Fastify({ logger: true });
-const prisma = new PrismaClient();
 
 fastify.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
   return { message: "Hello from Fastify and Prisma!" };
@@ -10,8 +8,8 @@ fastify.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
-    console.log("Server running at http://localhost:3000");
+    await fastify.listen({ port: 3001 });
+    console.log("Server running at http://localhost:3001");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
