cf6135f9d1b5788d71a6ebce0bb6aa6b7076807f
azuma-ya <kazu.bluemountain@gmail.com>
2024-09-21 18:12:37 +0900

    feat: ネットワークを設定する

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,12 @@ import Fastify, { FastifyRequest, FastifyReply } from "fastify";
 const fastify = Fastify({ logger: true });
 
 fastify.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
-  return { message: "Hello from Fastify and Prisma!" };
+  return { message: "Hello from Fastify and Prisma and Nextjs!" };
 });
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3001 });
-    console.log("Server running at http://localhost:3001");
+    await fastify.listen({ port: 3001, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
